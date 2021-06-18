59f4bcf598b1f88049de45d14fe336140da89166
YeonggilGo <dudrlf1859@naver.com>
2021-06-18 13:31:41 +0900

    test : Balls test , check answer for one Ball

---
+++ 1. UnitTestAfterFeedBack/src/test/java/Baseball/BallsTest.java
@@ -0,0 +1,32 @@
+package Baseball;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BallsTest {
+    Balls balls;
+
+    @BeforeEach
+    void setUp() {
+        balls = new Balls(Arrays.asList(1, 2, 3));
+    }
+
+    @Test
+    void nothing() {
+        assertThat(balls.play(new Ball(1, 4))).isEqualTo(BallStatus.NOTHING);
+    }
+
+    @Test
+    void ball() {
+        assertThat(balls.play(new Ball(0, 2))).isEqualTo(BallStatus.BALL);
+    }
+
+    @Test
+    void strike() {
+        assertThat(balls.play(new Ball(0, 1))).isEqualTo(BallStatus.STRIKE);
+    }
+}
