61d3237778364c2ca99818efc05354aea1b724b0
YeonggilGo <dudrlf1859@naver.com>
2021-06-19 13:43:08 +0900

    feat : calcul for 1 number input

--- 2. Racingcar/src/main/java/StringAddCalculator.java
+++ 2. Racingcar/src/main/java/StringAddCalculator.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+
 public class StringAddCalculator {
     public static int splitAndSum(String input) {
-        if (input == null || input.isEmpty()) return 0;
-        
-        return 0;
+        if (input == null || input.isEmpty())
+            return 0;
+
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .reduce(0, (fir, sec) -> fir + sec);
     }
 
 }
